a5f7f2722db3df44cb6c7a49eda1e73dea288e03
Toringol <noneoftoringol@gmail.com>
2020-04-28 00:58:36 +0300

    Start testing handlers but still error because of cookies logic(i think need to mock redis)

--- app/user/delivery/http/handlers_test.go
+++ app/user/delivery/http/handlers_test.go
@@ -1,1 +1,37 @@
 package http
+
+// Test no working because of cookie logic (need to mock redis i think)
+// func TestSignUp(t *testing.T) {
+// 	ctrl := gomock.NewController(t)
+// 	defer ctrl.Finish()
+
+// 	userCRUD := user.NewMockUsecase(ctrl)
+
+// 	var userJSON = `{"username": "hello", "password": "123"}`
+
+// 	userInput := &model.User{
+// 		Username: "hello",
+// 		Password: "123",
+// 		Avatar:   "avatars/defaultAvatar",
+// 		UserPersonalData: model.UserPersonalData{
+// 			DateOfBirth: time.Time{},
+// 		},
+// 	}
+
+// 	userCRUD.EXPECT().SelectUserByUsername("hello").Return(nil, nil)
+// 	userCRUD.EXPECT().CreateUser(userInput).Return(int64(0), nil)
+
+// 	handler := &userHandlers{
+// 		usecase: userCRUD,
+// 	}
+
+// 	e := echo.New()
+// 	req := httptest.NewRequest(http.MethodPost, "/signup/", strings.NewReader(userJSON))
+// 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+// 	rec := httptest.NewRecorder()
+// 	c := e.NewContext(req, rec)
+
+// 	if assert.NoError(t, handler.handleSignUp(c)) {
+// 		assert.Equal(t, http.StatusCreated, rec.Code)
+// 	}
+// }
